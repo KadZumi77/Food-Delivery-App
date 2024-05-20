50f06482b3786240deec77a88a2e3e32ac10204a
KadZumi77 <yazykova.2004@mail.ru>
2024-05-20 18:31:14 +0300

    Creat Main Page(Splash&Start Activity) + Login Page

---
+++ app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
@@ -0,0 +1,15 @@
+package com.example.fooddelivery
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fooddelivery.databinding.ActivityStartBinding
+
+class LoginUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
