f2330b9d06cbbbc504f391d849237d6c0f367b12
KadZumi77 <yazykova.2004@mail.ru>
2024-06-26 17:07:13 +0300

    Image Slider

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.fooddelivery
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.fooddelivery.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navigationView = findNavController(R.id.fragment_container)
+
+        bottomNavView.setupWithNavController(navigationView)
+
     }
 }
