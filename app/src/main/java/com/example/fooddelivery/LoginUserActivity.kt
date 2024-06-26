6fe56c64ab45df950126c04b85d47877a05fceb3
KadZumi77 <yazykova.2004@mail.ru>
2024-06-26 14:02:37 +0300

    Autorisation & Registration

--- app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
+++ app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
@@ -3,13 +3,27 @@ package com.example.fooddelivery
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fooddelivery.SignUpUserActivity
+import com.example.fooddelivery.databinding.ActivityLoginUserBinding
 import com.example.fooddelivery.databinding.ActivityStartBinding
 
 class LoginUserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.goSignUpUser.setOnClickListener{
+            val intent = Intent(this@LoginUserActivity, SignUpUserActivity::class.java)
+            startActivity(intent)
+        }
 
+        binding.button3.setOnClickListener {
+            val intent = Intent(this@LoginUserActivity, LocationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
